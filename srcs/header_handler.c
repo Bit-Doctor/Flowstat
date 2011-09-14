82ad99c58fba80f39eed75c3ae055388c98a78c7
Jonathan Machado <jonathan.machado@epitech.eu>
2011-09-14 14:41:47 +0200

    add input/output

--- srcs/header_handler.c
+++ srcs/header_handler.c
@@ -5,7 +5,7 @@
 ** Login   <jonathan.machado@epitech.net>
 **
 ** Started on  Wed Sep  7 14:28:37 2011 Jonathan Machado
-** Last update Wed Sep 14 13:43:59 2011 Jonathan Machado
+** Last update Wed Sep 14 14:41:25 2011 Jonathan Machado
 */
 
 #include <arpa/inet.h>
@@ -50,37 +50,36 @@ static void			icmpheader_handler(void *protocol_header, cson_object *new)
     }
 }
 
-static void			tcpheader_handler(void *protocol_header, cson_object *new)
+static void			tcpheader_handler(void *protocol_header, cson_object *new, int input)
 {
   struct tcphdr		*tcph;
 
   tcph = protocol_header;
-  cson_object_set(new, "port source", cson_value_new_integer(ntohs(tcph->source)));
-  cson_object_set(new, "port destination", cson_value_new_integer(ntohs(tcph->dest)));
+  if (input)
+    cson_object_set(new, "port", cson_value_new_integer(ntohs(tcph->source)));
+  else
+    cson_object_set(new, "port", cson_value_new_integer(ntohs(tcph->dest)));
 }
 
-static void			udpheader_handler(void *protocol_header, cson_object *new)
+static void			udpheader_handler(void *protocol_header, cson_object *new, int input)
 {
   struct udphdr		*udph;
 
   udph = protocol_header;
-  cson_object_set(new, "port source", cson_value_new_integer(ntohs(udph->source)));
-  cson_object_set(new, "port destination", cson_value_new_integer(ntohs(udph->dest)));
+  if (input)
+    cson_object_set(new, "port", cson_value_new_integer(ntohs(udph->source)));
+  else
+    cson_object_set(new, "port", cson_value_new_integer(ntohs(udph->dest)));
 }
 
-static void			create_new_connection_object(cson_array *connections,  struct iphdr *iph)
+static void			create_new_connection_object(cson_array *connections,  struct iphdr *iph, int input)
 {
-  int			addr;
-  char			*daddr;
   cson_value		*newV;
   cson_object		*new;
 
-  addr = ntohl(iph->daddr);
-  daddr = xmalloc(INET6_ADDRSTRLEN * sizeof(*daddr));
-  sprintf(daddr,"%u.%u.%u.%u", INTTOIP(addr));
   newV = cson_value_new_object();
   new = cson_value_get_object(newV);
-  cson_object_set(new, "ip destination", cson_value_new_string(daddr, strlen(daddr)));
+  cson_object_set(new, "direction", cson_value_new_integer(input));
   cson_object_set(new, "protocole number", cson_value_new_integer(iph->protocol));
   switch (iph->protocol)
     {
@@ -90,11 +89,11 @@ static void			create_new_connection_object(cson_array *connections,  struct iphd
       break;
     case IPPROTO_TCP:
       cson_object_set(new, "protocole name", cson_value_new_string("tcp", strlen("tcp")));
-      tcpheader_handler((u_int32_t *)iph + iph->ihl, new);
+      tcpheader_handler((u_int32_t *)iph + iph->ihl, new, input);
       break;
     case IPPROTO_UDP:
       cson_object_set(new, "protocole name", cson_value_new_string("udp", strlen("udp")));
-      udpheader_handler((u_int32_t *)iph + iph->ihl, new);
+      udpheader_handler((u_int32_t *)iph + iph->ihl, new, input);
       break;
     default:
       cson_object_set(new, "protocole name", cson_value_new_string("other", strlen("other")));
@@ -103,10 +102,9 @@ static void			create_new_connection_object(cson_array *connections,  struct iphd
   cson_object_set(new, "last log", cson_value_new_integer(time(NULL)));
   cson_object_set(new, "number of occurancy", cson_value_new_integer(1));
   cson_array_append(connections, newV);
-  free(daddr);
 }
 
-static void			create_new_flux_object(char *saddr, struct iphdr *iph)
+static void			create_new_flux_object(char *str_addr, struct iphdr *iph, int input)
 {
   cson_value		*newV;
   cson_object		*new;
@@ -117,7 +115,7 @@ static void			create_new_flux_object(char *saddr, struct iphdr *iph)
   char			dns[1024];
 
   socket.sin_family = AF_INET;
-  socket.sin_addr.s_addr = inet_addr(saddr);
+  socket.sin_addr.s_addr = inet_addr(str_addr);
   socket.sin_port = htons(80);
   getnameinfo(&socket, sizeof(socket), dns, sizeof(dns), NULL, 0, 0);
 #endif
@@ -125,7 +123,7 @@ static void			create_new_flux_object(char *saddr, struct iphdr *iph)
   new = cson_value_get_object(newV);
   connectionsV = cson_value_new_array();
   connections = cson_value_get_array(connectionsV);
-  cson_object_set(new, "ip source", cson_value_new_string(saddr, strlen(saddr)));
+  cson_object_set(new, "ip", cson_value_new_string(str_addr, strlen(str_addr)));
 #ifdef DNS_ACTIVATE
   cson_object_set(new, "hostname", cson_value_new_string(dns, strlen(dns)));
 #endif
@@ -133,7 +131,7 @@ static void			create_new_flux_object(char *saddr, struct iphdr *iph)
   cson_object_set(new, "last connection", cson_value_new_integer(time(NULL)));
   cson_object_set(new, "connections", connectionsV);
   cson_array_append(info.flux, newV);
-  create_new_connection_object(connections, iph);
+  create_new_connection_object(connections, iph, input);
 }
 
 static void			incr_connection_object(cson_object* object)
@@ -150,13 +148,15 @@ static void			incr_connection_object(cson_object* object)
   cson_object_set(object, "number of occurancy", cson_value_new_integer(prev_occ + 1));
 }
 
-static int			is_connection_already_listed(cson_object* object,  struct iphdr *iph)
+static int			is_the_same_connection(cson_object* object,  struct iphdr *iph, int input)
 {
   void				*protocol_header;
   struct tcphdr			*tcph;
   struct icmphdr		*icmph;
   struct udphdr			*udph;
 
+  if (input != cson_value_get_integer(cson_object_get(object, "direction")))
+    return (0);
   if (iph->protocol != cson_value_get_integer(cson_object_get(object, "protocole number")))
     return (0);
   protocol_header = (u_int32_t *)iph + iph->ihl;
@@ -169,16 +169,18 @@ static int			is_connection_already_listed(cson_object* object,  struct iphdr *ip
       break;
     case IPPROTO_TCP:
       tcph = protocol_header;
-      if (htons(tcph->source) != cson_value_get_integer(cson_object_get(object, "port source")) ||
-	  htons(tcph->dest) != cson_value_get_integer(cson_object_get(object, "port destination")))
+      if (input && htons(tcph->source) != cson_value_get_integer(cson_object_get(object, "port")))
+	return (0);
+      else if (htons(tcph->dest) != cson_value_get_integer(cson_object_get(object, "port")))
 	return (0);
       break;
     case IPPROTO_UDP:
       udph = protocol_header;
-      if (htons(udph->source) != cson_value_get_integer(cson_object_get(object, "port source")) ||
-	  htons(udph->dest) != cson_value_get_integer(cson_object_get(object, "port destination")))
+      if (input && htons(udph->source) != cson_value_get_integer(cson_object_get(object, "port")))
 	return (0);
-    }
+      else if (htons(udph->dest) != cson_value_get_integer(cson_object_get(object, "port")))
+	return (0);
+	}
   return (1);
 }
 
@@ -189,22 +191,29 @@ void			packet_handler(ulog_packet_msg_t *pkt)
   int			j;
   int			find;
   int			flux_len;
+  int			input;
   int			connections_len;
   int			addr;
-  char			*saddr;
+  char			*str_addr;
   struct iphdr		*iph;
   cson_object		*tempO;
   cson_array		*tempA;
 
+  input = 0;
   iph = (struct iphdr *)pkt->payload;
-  saddr = xmalloc(INET6_ADDRSTRLEN * sizeof(*saddr));
-  addr = ntohl(iph->saddr);
-  sprintf(saddr,"%u.%u.%u.%u", INTTOIP(addr));
+  str_addr = xmalloc(INET6_ADDRSTRLEN * sizeof(*str_addr));
+  addr = ntohl(iph->daddr);
+  if (ntohl(iph->daddr) == info.local_ip)
+    {
+      addr = ntohl(iph->saddr);
+      input = 1;
+    }
+  sprintf(str_addr,"%u.%u.%u.%u", INTTOIP(addr));
   flux_len = cson_array_length_get(info.flux);
   for (i = 0, find = 0; i < flux_len; ++i)
     {
       tempO = cson_value_get_object(cson_array_get(info.flux, i));
-      if (strcmp(cson_string_cstr(cson_value_get_string(cson_object_get(tempO, "ip source"))), saddr) == 0)
+      if (strcmp(cson_string_cstr(cson_value_get_string(cson_object_get(tempO, "ip"))), str_addr) == 0)
 	{
 	  cson_object_set(tempO, "last connection", cson_value_new_integer(time(NULL)));
 	  tempA =  cson_value_get_array(cson_object_get(tempO, "connections"));
@@ -212,20 +221,20 @@ void			packet_handler(ulog_packet_msg_t *pkt)
 	  for (j = 0; j < connections_len; ++j)
 	    {
 	      tempO = cson_value_get_object(cson_array_get(tempA, j));
-	      if (is_connection_already_listed(tempO, iph))
+	      if (is_the_same_connection(tempO, iph, input))
 		{
 		  incr_connection_object(tempO);
 		  find = 1;
 		}
 	    }
 	  if (!find)
-	    create_new_connection_object(tempA, iph);
+	    create_new_connection_object(tempA, iph, input);
 	  find = 1;
 	}
     }
   if (!find)
-    create_new_flux_object(saddr, iph);
-  free(saddr);
+    create_new_flux_object(str_addr, iph, input);
+  free(str_addr);
 }
 
 #endif /* DEBUG */
